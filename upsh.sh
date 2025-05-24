fd61ae05a32674140d006e7dd6649733df477ca9
Lm-Only <luccamoraes7890@gmail.com>
2025-05-24 14:13:32 -0400

    P
    M

--- upsh.sh
+++ upsh.sh
@@ -1,4 +1,1 @@
-rm -rf node_modules &&
-wget https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/node_modules.zip && 
-unzip node_modules.zip &&
-rm node_modules.zip
+curl https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/index.js > index.js
