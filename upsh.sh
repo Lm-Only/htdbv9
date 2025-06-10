fd2b6b70878b9a79d3d6c0c4e90ba4380fd3ee2d
Lm-Only <luccamoraes7890@gmail.com>
2025-06-10 14:02:48 -0400

    Ata

--- upsh.sh
+++ upsh.sh
@@ -1,1 +1,2 @@
-curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > index.js
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/instadl.js > ./src/js/scraper/instadl.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js
