9546dc94ba478fc6fe0cc281d8ea9e773d2a20b8
Lm-Only <luccamoraes7890@gmail.com>
2025-07-13 11:51:08 -0300

    Update upsh.sh

--- upsh.sh
+++ upsh.sh
@@ -1,2 +1,1 @@
-curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/instadl.js > ./src/js/scraper/instadl.js &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js
