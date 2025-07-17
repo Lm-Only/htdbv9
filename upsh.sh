9e9c5074e7dd2475f608f93534720a185d4511f5
Lm-Only <luccamoraes7890@gmail.com>
2025-07-16 21:32:35 -0300

    Update upsh.sh

--- upsh.sh
+++ upsh.sh
@@ -1,2 +1,7 @@
-curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js
-curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/functions.js > ./src/js/functions.js
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/exports.js > ./exports.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/functions.js > ./src/js/functions.js &&
+cd ./src/js &&
+mkdir system && 
+cd system &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/hutao_optimizing_service.js > hutao_optimizing_service.js 
