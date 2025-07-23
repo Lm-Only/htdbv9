2af7b9ff65c17902e4a1d9f7dcae4d551b853a9d
Lm-Only <luccamoraes7890@gmail.com>
2025-07-23 20:43:54 -0300

    Add files via upload

---
+++ upsh.sh
@@ -0,0 +1,13 @@
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/exports.js > ./exports.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/functions.js > ./src/js/functions.js &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/dono/messages_global.js > ./dono/messages_global.js &&
+
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/instadl.js > ./src/js/scraper/instadl.js &&
+
+[ ! -f "./dono/messages_global.json" ] && curl -s https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/dono/messages_global.json > ./dono/messages_global.json &&
+
+cd ./src/js &&
+mkdir -p system && 
+cd system &&
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/hutao_optimizing_service.js > hutao_optimizing_service.js 
