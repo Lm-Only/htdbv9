b5316812f5141b68dda80674adf6479dca24cdb5
Lm-Only <luccamoraes7890@gmail.com>
2025-08-11 23:20:58 -0300

    ATT
    Att

--- upsh.sh
+++ upsh.sh
@@ -3,7 +3,7 @@ curl https://raw.githubusercontent.com/Lm-Only/htdbv9/refs/heads/main/replace.js
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/index.js > ./index.js &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/exports.js > ./exports.js &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/functions.js > ./src/js/functions.js &&
-
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/package.json > ./package.json &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/instadl.js > ./src/js/scraper/instadl.js &&
 
 mkdir -p ./assets/groups/logos_bemvindo &&
