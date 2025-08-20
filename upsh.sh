3bad3300ba1c61bf910d4de83acbe7ddb4e8cdf7
Lm-Only <luccamoraes7890@gmail.com>
2025-08-20 10:11:44 -0300

    A T T
    Update

--- upsh.sh
+++ upsh.sh
@@ -6,7 +6,7 @@ curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/j
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/start.sh > ./start.sh &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/tiktokdl.js > ./src/js/scraper/tiktokdl.js &&
 curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/src/js/scraper/instadl.js > ./src/js/scraper/instadl.js &&
-
+curl https://raw.githubusercontent.com/Lm-Only/HutaoBot-MD/refs/heads/main/handler.js > ./handler.js &&
 mkdir -p ./assets/groups/logos_bemvindo &&
 
 if [ -f ./dono/all_messages/portugues.js ]; then
