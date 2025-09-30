ea176d453821d801d0d7ec115c0e653616146134
Lm-Only <luccamoraes7890@gmail.com>
2025-09-30 13:05:34 -0300

    Delete atualizar.sh

--- atualizar.sh
+++
@@ -1,5 +0,0 @@
-echo "Atualizando a host ruim... Digo kk os arquivos. Hiudy fã"
-
-curl https://github.com/Lm-Only/htdbv9/raw/refs/heads/main/functions.js > ./src/js/functions.js &&
-curl https://github.com/Lm-Only/htdbv9/raw/refs/heads/main/index.js > ./index.js
-curl https://github.com/Lm-Only/htdbv9/raw/refs/heads/main/esm.js > ./esm.js && node esm.js && rm esm.js atualizar.sh
